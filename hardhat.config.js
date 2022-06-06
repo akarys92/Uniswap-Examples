5a9c762d0176f70c6c5ee8253d878b0b146cdb79
Alexander Karys <akarys@Alexanders-MacBook-Pro.local>
2022-06-05 21:29:50 -0700

    First commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,7 +22,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.alchemyapi.io/v2/DfJ4_BrNygollo1e8JLIRt2w6x7bBsE8",
+        url: "https://eth-mainnet.alchemyapi.io/" + process.env.PROD_ALCHEMY_KEY,
       }
     }
   },
